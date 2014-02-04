4f34420ea0f18a5f608ffdb32fe01448f10283d3
Pannous <info@pannous.net>
2014-02-04 12:57:50 +0100

    ''

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-mvn install -U
+mvn install -U -DskipTests -Dmaven.test.skip=true
 mvn jar:jar
 cp target/blueprints-netbase-2.4.0.jar lib/
 cd lib; java -cp lib/*.jar -jar blueprints-netbase-2.4.0.jar 
